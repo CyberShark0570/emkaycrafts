b6807e533091d75ca5ad0d5cf2f6159c59758926
CyberShark0570 <manivasagan1712@gmail.com>
2022-05-29 11:57:04 +0530

    mongodb CRUD working

---
+++ emkaycrafts-backend/src/test/java/com/emkaycraftsbackend/EmkaycraftsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emkaycraftsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmkaycraftsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
